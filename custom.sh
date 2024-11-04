49cd16d939bb041af890cc4c3e58fcb21ff727a0
windrua <34590060+sfwtw@users.noreply.github.com>
2024-11-04 21:56:04 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -12,11 +12,12 @@
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.5.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.5.2.1/g' package/base-files/files/bin/config_generate
  
 # add custom packages
 # echo 'src-git xYx https://github.com/xYx-c/openwrt-luci' >>feeds.conf.default
-
+sed -i '$a src-git xmpackage https://github.com/xiaomeng9597/openwrt-packages2.git;main' feeds.conf.default
+sed -i '$a src-git istore https://github.com/linkease/istore.git;main' feeds.conf.default
 # openclash packages
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/openclash
 mv openclash/luci-app-openclash package/luci-app-openclash && rm -rf openclash

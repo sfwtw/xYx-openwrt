e54e08f3f0727622d0f90034ae323a56ccd17e01
windrua <34590060+sfwtw@users.noreply.github.com>
2024-11-04 21:57:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # Modify default IP
-sed -i 's/192.168.1.1/10.5.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.5.2.1/g' package/base-files/files/bin/config_generate
  
 # add custom packages
 # echo 'src-git xYx https://github.com/xYx-c/openwrt-luci' >>feeds.conf.default
